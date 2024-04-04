5c18e82fc0a1c737577af8b10018453b3069fa1e
Devmekus <nnaji.nnaemeka712@gmail.com>
2024-04-04 02:18:44 -0700

    100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,5 +1,6 @@
-
-n Deletes out-of-date dArchives
+#!/usr/bin/python3
+"""
+Function Deletes out-of-date archives
 fab -f 100-clean_web_static.py do_clean:number=2
     -i ssh-key -u ubuntu > /dev/null 2>&1
 """
@@ -7,52 +8,24 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['100.26.57.130', '54.227.221.235']
+env.hosts = ['52.87.155.66', '54.89.109.87']
 
 
 def do_clean(number=0):
-    """function Delete out-of-date archives.
+    """Fnction Delete out-of-date archives.
     Args:
-        number (int): The number of archives to keep.    
+        number (int): The number of archives to keep.
+    
     """
     number = 1 if int(number) == 0 else int(number)
 
-    dArchives = sorted(os.listdir("versions"))
-    [dArchives.pop() for i in range(number)]
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in dArchives]
+        [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
-        dArchives = run("ls -tr").split()
-        dArchives = [a for a in dArchives if "web_static_" in a]
-        [dArchives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in dArchives]#!/usr/bin/python3
-"""
-Function Deletes out-of-date dArchives
-fab -f 100-clean_web_static.py do_clean:number=2
-    -i ssh-key -u ubuntu > /dev/null 2>&1
-"""
-
-import os
-from fabric.api import *
-
-env.hosts = ['100.26.57.130', '54.227.221.235']
-
-
-def do_clean(number=0):
-    """function Delete out-of-date archives.
-    Args:
-        number (int): The number of archives to keep.    
-    """
-    number = 1 if int(number) == 0 else int(number)
-
-    dArchives = sorted(os.listdir("versions"))
-    [dArchives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in dArchives]
-
-    with cd("/data/web_static/releases"):
-        dArchives = run("ls -tr").split()
-        dArchives = [a for a in dArchives if "web_static_" in a]
-        [dArchives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in dArchives]
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
