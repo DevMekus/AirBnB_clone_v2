ea7bfa8100869414f0144c226bc2f19fd3ab273e
Devmekus <nnaji.nnaemeka712@gmail.com>
2024-04-04 02:12:50 -0700

    Web static deployment

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,30 +1,58 @@
-#!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
+
+n Deletes out-of-date dArchives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+
 import os
 from fabric.api import *
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ['100.26.57.130', '54.227.221.235']
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
-
+    """function Delete out-of-date archives.
     Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
+        number (int): The number of archives to keep.    
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    dArchives = sorted(os.listdir("versions"))
+    [dArchives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local("rm ./{}".format(a)) for a in dArchives]
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        dArchives = run("ls -tr").split()
+        dArchives = [a for a in dArchives if "web_static_" in a]
+        [dArchives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in dArchives]#!/usr/bin/python3
+"""
+Function Deletes out-of-date dArchives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
+"""
+
+import os
+from fabric.api import *
+
+env.hosts = ['100.26.57.130', '54.227.221.235']
+
+
+def do_clean(number=0):
+    """function Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.    
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    dArchives = sorted(os.listdir("versions"))
+    [dArchives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in dArchives]
+
+    with cd("/data/web_static/releases"):
+        dArchives = run("ls -tr").split()
+        dArchives = [a for a in dArchives if "web_static_" in a]
+        [dArchives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in dArchives]
